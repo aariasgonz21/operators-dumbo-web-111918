a10c12765c41cc8cf7f7d7885a8df258981174b8
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-11-04 01:20:28 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,16 @@
 def unsafe?(speed)
-  speed < 40 || speed > 60 ? true : false
-end
-
-
-
-def not_safe?(speed)
-	if(speed < 40 || speed > 60)
+  if(speed < 40 || speed > 60)
 	  true
 	 else
 	   false
 	 end
+end
+  
+
+
+
+def not_safe?(speed)
+	speed < 40 || speed > 60 ? true : false
 end
 	
 
